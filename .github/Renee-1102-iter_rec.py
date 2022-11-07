c4206af8a150097ffbc857d4448d054b155c8d9c
Renee1102 <112365287+Renee1102@users.noreply.github.com>
2022-11-07 11:15:57 +0700

    .

---
+++ .github/Renee-1102-iter_rec.py
@@ -0,0 +1,14 @@
+'''
+Pseudo Code 
+Define base and exponent inside iterative power 
+Set base to be either interger or float value 
+Set exponent to only interger value bigger or equal to 0
+Return the value of base with the exponent in either interger form of float form
+'''
+
+def iterative_power(base, exp): 
+    base = float(input("base: "))
+    exp >= 0
+    return base * base * exp
+
+    
