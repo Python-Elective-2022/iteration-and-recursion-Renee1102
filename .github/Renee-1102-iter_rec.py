b2178e5952c233630449f7e3087782b4848060fe
Renee1102 <112365287+Renee1102@users.noreply.github.com>
2022-11-17 17:45:17 +0700

    done

--- .github/Renee-1102-iter_rec.py
+++ .github/Renee-1102-iter_rec.py
@@ -6,9 +6,28 @@ Set exponent to only interger value bigger or equal to 0
 Return the value of base with the exponent in either interger form of float form
 '''
 
-def iterative_power(base, exp): 
-    base = float(input("base: "))
-    exp >= 0
-    return base * base * exp
+base = float(input("base: "))
+exp = int(input("exp: "))
 
-    
+def iterative_power(base, exp): 
+    for i in range(exp):
+        i = base * exp
+    return i
+
+print(iterative_power(base, exp))
+
+
+'''
+Pseudo Code 
+Define base and exponent inside recursive power 
+Set base to be either interger or float value 
+Set exponent to only interger value bigger or equal to 0
+Return the value of base with the exponent in either interger form of float form
+'''
+
+def recursive_power(base, exp):
+    if exp >=0:
+        n = base * exp 
+        return n 
+
+print(recursive_power(base, exp))
